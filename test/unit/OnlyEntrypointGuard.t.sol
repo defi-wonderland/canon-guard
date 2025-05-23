e33b71f09ba802d12ce4686302f78d753b255416
lempire123 <61431140+lempire123@users.noreply.github.com>
2025-05-23 17:41:53 +0200

    fix: re-add bulloak check

--- test/unit/OnlyEntrypointGuard.t.sol
+++ test/unit/OnlyEntrypointGuard.t.sol
@@ -8,21 +8,6 @@ import {Test} from 'forge-std/Test.sol';
 import {IOnlyEntrypointGuard} from 'interfaces/IOnlyEntrypointGuard.sol';
 
 contract UnitOnlyEntrypointGuard is Test {
-  OnlyEntrypointGuardForTest onlyEntrypointGuard;
-  address public immutable ENTRYPOINT = makeAddr('ENTRYPOINT');
-  address public immutable EMERGENCY_CALLER = makeAddr('EMERGENCY_CALLER');
-  address public immutable MULTI_SEND_CALL_ONLY = makeAddr('MULTI_SEND_CALL_ONLY');
-
-  function setUp() public {
-    onlyEntrypointGuard = new OnlyEntrypointGuardForTest(ENTRYPOINT, EMERGENCY_CALLER, MULTI_SEND_CALL_ONLY);
-  }
-
-  function _assumeFuzzable(address _address) internal pure {
-    assumeNotForgeAddress(_address);
-    assumeNotZeroAddress(_address);
-    assumeNotPrecompile(_address);
-  }
-
   function test_ConstructorWhenPassingValidParameters(
     address _entrypoint,
     address _emergencyCaller,
@@ -41,118 +26,138 @@ contract UnitOnlyEntrypointGuard is Test {
   }
 
   modifier whenCallerIsEntrypoint() {
+    vm.prank(ENTRYPOINT);
     _;
   }
 
-  function test_CheckTransactionWhenCallerIsEntrypointWhenOperationIsCall() external whenCallerIsEntrypoint {
+  function test_CheckTransactionWhenOperationIsEntrypointCall() external whenCallerIsEntrypoint {
     onlyEntrypointGuard.checkTransaction(
-      address(0), 0, bytes(''), Enum.Operation.Call, 0, 0, 0, address(0), payable(address(0)), bytes(''), ENTRYPOINT
+      address(0), 0, '', Enum.Operation.Call, 0, 0, 0, address(0), payable(address(0)), '', ENTRYPOINT
     );
   }
 
-  function test_CheckTransactionWhenCallerIsEntrypointWhenOperationIsDelegateCallWhenTargetIsMultiSendCallOnly()
+  modifier whenOperationIsEntrypointDelegateCall() {
+    _;
+  }
+
+  function test_CheckTransactionWhenTargetIsEntrypointMultiSendCallOnly()
     external
     whenCallerIsEntrypoint
+    whenOperationIsEntrypointDelegateCall
   {
     onlyEntrypointGuard.checkTransaction(
-      MULTI_SEND_CALL_ONLY,
-      0,
-      bytes(''),
-      Enum.Operation.DelegateCall,
-      0,
-      0,
-      0,
-      address(0),
-      payable(address(0)),
-      bytes(''),
-      ENTRYPOINT
+      MULTI_SEND_CALL_ONLY, 0, '', Enum.Operation.DelegateCall, 0, 0, 0, address(0), payable(address(0)), '', ENTRYPOINT
     );
   }
 
-  function test_CheckTransactionWhenCallerIsEntrypointWhenOperationIsDelegateCallWhenTargetIsNotMultiSendCallOnly(
-    address _target
-  ) external whenCallerIsEntrypoint {
-    vm.assume(_target != MULTI_SEND_CALL_ONLY);
-    vm.expectRevert(abi.encodeWithSelector(IOnlyEntrypointGuard.UnauthorizedDelegateCall.selector, _target));
+  function test_CheckTransactionWhenTargetIsNotEntrypointMultiSendCallOnly()
+    external
+    whenCallerIsEntrypoint
+    whenOperationIsEntrypointDelegateCall
+  {
+    vm.expectRevert(abi.encodeWithSelector(IOnlyEntrypointGuard.UnauthorizedDelegateCall.selector, address(0)));
     onlyEntrypointGuard.checkTransaction(
-      _target,
-      0,
-      bytes(''),
-      Enum.Operation.DelegateCall,
-      0,
-      0,
-      0,
-      address(0),
-      payable(address(0)),
-      bytes(''),
-      ENTRYPOINT
+      address(0), 0, '', Enum.Operation.DelegateCall, 0, 0, 0, address(0), payable(address(0)), '', ENTRYPOINT
     );
   }
 
   modifier whenCallerIsEmergencyCaller() {
+    vm.prank(EMERGENCY_CALLER);
     _;
   }
 
-  function test_CheckTransactionWhenCallerIsEmergencyCallerWhenOperationIsCall() external whenCallerIsEmergencyCaller {
+  function test_CheckTransactionWhenOperationIsEmergencyCall() external whenCallerIsEmergencyCaller {
     onlyEntrypointGuard.checkTransaction(
-      address(0),
-      0,
-      bytes(''),
-      Enum.Operation.Call,
-      0,
-      0,
-      0,
-      address(0),
-      payable(address(0)),
-      bytes(''),
-      EMERGENCY_CALLER
+      address(0), 0, '', Enum.Operation.Call, 0, 0, 0, address(0), payable(address(0)), '', EMERGENCY_CALLER
     );
   }
 
-  function test_CheckTransactionWhenCallerIsEmergencyCallerWhenOperationIsDelegateCallWhenTargetIsMultiSendCallOnly()
+  modifier whenOperationIsEmergencyDelegateCall() {
+    _;
+  }
+
+  function test_CheckTransactionWhenTargetIsEmergencyMultiSendCallOnly()
     external
     whenCallerIsEmergencyCaller
+    whenOperationIsEmergencyDelegateCall
   {
     onlyEntrypointGuard.checkTransaction(
       MULTI_SEND_CALL_ONLY,
       0,
-      bytes(''),
+      '',
       Enum.Operation.DelegateCall,
       0,
       0,
       0,
       address(0),
       payable(address(0)),
-      bytes(''),
+      '',
       EMERGENCY_CALLER
     );
   }
 
-  function test_CheckTransactionWhenCallerIsEmergencyCallerWhenOperationIsDelegateCallWhenTargetIsNotMultiSendCallOnly(
-    address _target
-  ) external whenCallerIsEmergencyCaller {
-    vm.assume(_target != MULTI_SEND_CALL_ONLY);
-    vm.expectRevert(abi.encodeWithSelector(IOnlyEntrypointGuard.UnauthorizedDelegateCall.selector, _target));
+  function test_CheckTransactionWhenTargetIsNotEmergencyMultiSendCallOnly()
+    external
+    whenCallerIsEmergencyCaller
+    whenOperationIsEmergencyDelegateCall
+  {
+    vm.expectRevert(abi.encodeWithSelector(IOnlyEntrypointGuard.UnauthorizedDelegateCall.selector, address(0)));
     onlyEntrypointGuard.checkTransaction(
-      _target,
+      address(0), 0, '', Enum.Operation.DelegateCall, 0, 0, 0, address(0), payable(address(0)), '', EMERGENCY_CALLER
+    );
+  }
+
+  function test_CheckTransactionWhenCallerIsNotEntrypointOrEmergencyCaller() external {
+    vm.prank(address(0));
+    vm.expectRevert(abi.encodeWithSelector(IOnlyEntrypointGuard.UnauthorizedSender.selector, address(0)));
+    onlyEntrypointGuard.checkTransaction(
+      address(0), 0, '', Enum.Operation.Call, 0, 0, 0, address(0), payable(address(0)), '', address(0)
+    );
+  }
+
+  // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+  // ==================== BULLOAK AUTOGENERATED SEPARATOR ====================
+  // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  //    Code below this section could not be automatically moved by bulloak
+  // =========================================================================
+
+  // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+  // ==================== BULLOAK AUTOGENERATED SEPARATOR ====================
+  // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  //    Code below this section could not be automatically moved by bulloak
+  // =========================================================================
+  OnlyEntrypointGuardForTest onlyEntrypointGuard;
+  address public immutable ENTRYPOINT = makeAddr('ENTRYPOINT');
+  address public immutable EMERGENCY_CALLER = makeAddr('EMERGENCY_CALLER');
+  address public immutable MULTI_SEND_CALL_ONLY = makeAddr('MULTI_SEND_CALL_ONLY');
+
+  function setUp() public {
+    onlyEntrypointGuard = new OnlyEntrypointGuardForTest(ENTRYPOINT, EMERGENCY_CALLER, MULTI_SEND_CALL_ONLY);
+  }
+
+  function _assumeFuzzable(address _address) internal pure {
+    assumeNotForgeAddress(_address);
+    assumeNotZeroAddress(_address);
+    assumeNotPrecompile(_address);
+  }
+
+  function test_CheckTransactionWhenOperationIsEmergencyDelegateCall()
+    external
+    whenCallerIsEmergencyCaller
+    whenOperationIsEmergencyDelegateCall
+  {
+    onlyEntrypointGuard.checkTransaction(
+      MULTI_SEND_CALL_ONLY,
       0,
-      bytes(''),
+      '',
       Enum.Operation.DelegateCall,
       0,
       0,
       0,
       address(0),
       payable(address(0)),
-      bytes(''),
+      '',
       EMERGENCY_CALLER
-    );
-  }
-
-  function test_CheckTransactionWhenCallerIsNotEntrypointOrEmergencyCaller(address _caller) external {
-    vm.assume(_caller != ENTRYPOINT && _caller != EMERGENCY_CALLER);
-    vm.expectRevert(abi.encodeWithSelector(IOnlyEntrypointGuard.UnauthorizedSender.selector, _caller));
-    onlyEntrypointGuard.checkTransaction(
-      address(0), 0, bytes(''), Enum.Operation.Call, 0, 0, 0, address(0), payable(address(0)), bytes(''), _caller
     );
   }
 }
