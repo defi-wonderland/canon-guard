02ba026c4e24328ae18b91e151fc9b874a3735b8
0xAustrian <114922365+0xAustrian@users.noreply.github.com>
2025-06-04 17:48:36 -0300

    chore: remove unused import

--- test/unit/AllowanceClaimorFactory.t.sol
+++ test/unit/AllowanceClaimorFactory.t.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.29;
 
-import {AllowanceClaimor} from 'contracts/actions-builders/AllowanceClaimor.sol';
 import {AllowanceClaimorFactory} from 'contracts/factories/AllowanceClaimorFactory.sol';
 import {Test} from 'forge-std/Test.sol';
 import {IAllowanceClaimor} from 'interfaces/actions-builders/IAllowanceClaimor.sol';
