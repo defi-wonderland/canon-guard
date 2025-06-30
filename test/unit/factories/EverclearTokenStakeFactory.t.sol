618695b0309a0e0d9260abf86d91b415b25b744b
0xAustrian <114922365+0xAustrian@users.noreply.github.com>
2025-06-30 13:37:43 -0300

    feat: add factories for new actions-builders

---
+++ test/unit/factories/EverclearTokenStakeFactory.t.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.29;
+
+import {Test} from 'forge-std/Test.sol';
+import {EverclearTokenStakeFactory} from 'src/contracts/factories/EverclearTokenStakeFactory.sol';
+import {IEverclearTokenStake} from 'src/interfaces/actions-builders/IEverclearTokenStake.sol';
+
+contract UnitEverclearTokenStakeFactorycreateEverclearTokenStake is Test {
+  EverclearTokenStakeFactory public everclearTokenStakeFactory;
+  IEverclearTokenStake public auxEverclearTokenStake;
+
+  function setUp() external {
+    everclearTokenStakeFactory = new EverclearTokenStakeFactory();
+  }
+
+  function test_WhenCalled(
+    address _vestingEscrow,
+    address _vestingWallet,
+    address _spokeBridge,
+    address _clearLockbox,
+    address _next,
+    address _clear,
+    address _safe,
+    uint256 _lockTime
+  ) external {
+    address _everclearTokenStake = everclearTokenStakeFactory.createEverclearTokenStake(
+      _vestingEscrow, _vestingWallet, _spokeBridge, _clearLockbox, _next, _clear, _safe, _lockTime
+    );
+
+    // it should deploy a EverclearTokenStake
+    auxEverclearTokenStake = IEverclearTokenStake(
+      deployCode(
+        'EverclearTokenStake',
+        abi.encode(_vestingEscrow, _vestingWallet, _spokeBridge, _clearLockbox, _next, _clear, _safe, _lockTime)
+      )
+    );
+
+    // it should match the parameters sent to the constructor
+    assertEq(address(IEverclearTokenStake(_everclearTokenStake).VESTING_ESCROW()), _vestingEscrow);
+    assertEq(address(IEverclearTokenStake(_everclearTokenStake).VESTING_WALLET()), _vestingWallet);
+    assertEq(address(IEverclearTokenStake(_everclearTokenStake).SPOKE_BRIDGE()), _spokeBridge);
+    assertEq(address(IEverclearTokenStake(_everclearTokenStake).CLEAR_LOCKBOX()), _clearLockbox);
+    assertEq(address(IEverclearTokenStake(_everclearTokenStake).NEXT()), _next);
+    assertEq(address(IEverclearTokenStake(_everclearTokenStake).CLEAR()), _clear);
+    assertEq(address(IEverclearTokenStake(_everclearTokenStake).SAFE()), _safe);
+    assertEq(IEverclearTokenStake(_everclearTokenStake).LOCK_TIME(), _lockTime);
+  }
+}
